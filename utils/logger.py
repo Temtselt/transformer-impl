f750bd77d327473baeef44fdfc6110ae3d8f3d3f
temuqile001 <temuqile001@ke.com>
2022-06-06 15:01:29 +0800

    Black format

--- utils/logger.py
+++ utils/logger.py
@@ -2,32 +2,38 @@ import datetime
 
 
 class Logger:
-
     @staticmethod
-    def log(tag: str, message: str, level: str = 'LOG'):
-        print(f'{datetime.datetime.now()} | {level:7s} | {tag}: {message}')
+    def log(tag: str, message: str, level: str = "LOG"):
+        print(f"{datetime.datetime.now()} | {level:7s} | {tag}: {message}")
 
     @staticmethod
     def logv(tag: str, message: str):
-        Logger.log(tag, message, 'VERBOSE')
+        Logger.log(tag, message, "VERBOSE")
 
     @staticmethod
     def logd(tag: str, message: str):
-        Logger.log(tag, message, 'DEBUG')
+        Logger.log(tag, message, "DEBUG")
 
     @staticmethod
     def logi(tag: str, message: str):
-        Logger.log(tag, message, 'INFO')
+        Logger.log(tag, message, "INFO")
 
     @staticmethod
     def logw(tag: str, message: str):
-        Logger.log(tag, message, 'WARN')
+        Logger.log(tag, message, "WARN")
 
     @staticmethod
     def loge(tag: str, message: str):
-        Logger.log(tag, message, 'ERROR')
+        Logger.log(tag, message, "ERROR")
 
 
-if __name__ == '__main__':
-    for l in (Logger.log, Logger.logv, Logger.logd, Logger.logi, Logger.logw, Logger.loge):
-        l('foo', 'bar')
+if __name__ == "__main__":
+    for l in (
+        Logger.log,
+        Logger.logv,
+        Logger.logd,
+        Logger.logi,
+        Logger.logw,
+        Logger.loge,
+    ):
+        l("foo", "bar")
