c74a973297ad4b7307ff5ecad0b4325ec76d57b0
temuqile001 <temuqile001@ke.com>
2022-06-07 17:06:58 +0800

    Add args

--- train.py
+++ train.py
@@ -1,23 +1,62 @@
 import copy
+import os
 import time
+from argparse import Namespace
 
+import torch
 import torch.nn as nn
 
 from transformer.embeddings import Embeddings
-from transformer.encoder_decoder import (
-    Decoder,
-    DecoderLayer,
-    Encoder,
-    EncoderDecoder,
-    EncoderLayer,
-)
+from transformer.encoder_decoder import (Decoder, DecoderLayer, Encoder,
+                                         EncoderDecoder, EncoderLayer)
 from transformer.generator import Generator
 from transformer.multi_head_attention import MultiHeadedAttention
 from transformer.positionwise_feed_forward import PositionwiseFeedForward
 from transformer.postional_encoding import PositionalEncoding
+from utils.helpers import handle_dirs, set_seed_everywhere
+from utils.logger import Logger
+
+args = Namespace(
+    dataset_csv="data/lyrics_lite.csv",
+    vectorizer_file="vectorizer.json",
+    model_state_file="model.pth",
+    save_dir="model.storage/cyrillc_to_mongolian",
+    reload_from_files=True,
+    expand_filepaths_to_save_dir=True,
+    cuda=False,
+    seed=1337,
+    learning_rate=5e-4,
+    batch_size=8,
+    num_epochs=100,
+    early_stopping_criteria=5,
+    source_embedding_size=32,
+    target_embedding_size=32,
+    encoding_size=32,
+    catch_keyboard_interrupt=True,
+)
 
 
 def make_model(src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
+    if args.expand_filepaths_to_save_dir:
+        args.vectorizer_file = os.path.join(args.save_dir, args.vectorizer_file)
+        args.model_state_file = os.path.join(args.save_dir, args.model_state_file)
+        Logger.logi(
+            __name__,
+            "Expanded filepaths: \n"
+            f"\t{args.vectorizer_file}\n"
+            f"\t{args.model_state_file}",
+        )
+
+    if not torch.cuda.is_available():
+        args.cuda = False
+        Logger.logi(__name__, "CUDA is not available")
+
+    args.device = torch.device("cuda" if args.cuda else "cpu")
+    Logger.logi(__name__, f"Using CUDA: {args.cuda}")
+
+    set_seed_everywhere(args.seed, args.cuda)
+    handle_dirs(args.save_dir)
+
     c = copy.deepcopy
     attn = MultiHeadedAttention(h, d_model)
     ff = PositionwiseFeedForward(d_model, d_ff, dropout)
