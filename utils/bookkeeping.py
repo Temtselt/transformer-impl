80c1625427acf4cd61b6aa262119c9934b612d35
temuqile001 <temuqile001@ke.com>
2022-06-07 16:18:32 +0800

    First commit

---
+++ utils/bookkeeping.py
@@ -0,0 +1,63 @@
+import torch
+
+
+def make_train_state(args):
+    return {
+        "stop_early": False,
+        "early_stopping_step": 0,
+        "early_stopping_best_val": 1e8,
+        "learning_rate": args.learning_rate,
+        "epoch_index": 0,
+        "train_loss": [],
+        "train_acc": [],
+        "val_loss": [],
+        "val_acc": [],
+        "test_loss": -1,
+        "test_acc": -1,
+        "model_filename": args.model_state_file,
+    }
+
+
+def update_train_state(args, model, train_state):
+    """Handle the training state updates.
+    Components:
+     - Early Stopping: Prevent overfitting.
+     - Model Checkpoint: Model is saved if the model is better
+
+    :param args: main arguments
+    :param model: model to train
+    :param train_state: a dictionary representing the training state values
+    :returns:
+        a new train_state
+    """
+
+    # Save one model at least
+    if train_state["epoch_index"] == 0:
+        torch.save(model.state_dict(), train_state["model_filename"])
+        train_state["stop_early"] = False
+
+    # Save model if perfomance improved
+    elif train_state["epoch_index"] >= 1:
+        loss_tm1, loss_t = train_state["val_loss"][-2:]
+
+        # If loss worsended
+        if loss_t > loss_tm1:
+            # Updata step:
+            train_state["early_stopping_step"] += 1
+
+        # Loss decreased
+        else:
+            # Save the best model
+            if loss_t < train_state["early_stopping_best_val"]:
+                torch.save(model.state_dict(), train_state["model_filename"])
+                train_state["early_stopping_best_val"] = loss_t
+
+            # Reset early stopping step
+            train_state["early_stopping_step"] = 0
+
+        # Stop early?
+        train_state["stop_early"] = (
+            train_state["early_stopping_step"] >= args.early_stopoing_criteria
+        )
+
+    return train_state
